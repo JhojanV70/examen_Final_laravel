1c3868e963ce937e0e5afc725e4490de3e205b94
JhojanV70 <jhojanv70@gmail.com>
2023-05-15 11:54:19 -0500

    create customers tabel

---
+++ database/migrations/2023_05_15_161141_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->integer('document_number');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address',80);
+            $table->date('birthday')->nullable();
+            $table->integer('phone_number');
+            $table->string('email',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
